24377d492bf303c516602d3d9ce5b4dd618c3536
tonmoy <tonmoyth13@gmail.com>
2025-05-19 15:27:41 +0600

    added user delete functionality

--- src/Pages/Users.jsx
+++ src/Pages/Users.jsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { GrUpdate } from "react-icons/gr";
 import { MdDelete, MdEdit } from "react-icons/md";
 import { Link, useLoaderData } from "react-router";
+import Swal from "sweetalert2";
 
 const Users = () => {
-  const usersData = useLoaderData();
+  const InitialUsersData = useLoaderData();
+  const [users,setUsers] = useState(InitialUsersData)
 
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/users/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              const reminingUsers = users.filter(user => user._id !== id);
+              setUsers(reminingUsers)
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
   return (
     <div>
       <div className="overflow-x-auto">
@@ -23,7 +55,7 @@ const Users = () => {
             </tr>
           </thead>
           <tbody>
-            {usersData.map((user, index) => (
+            {users.map((user, index) => (
               <tr key={user._id}>
                 <th>
                   <label>{index + 1}</label>
@@ -51,9 +83,16 @@ const Users = () => {
                 <th>
                   <div className="join space-x-4">
                     <Link to={`/update/${user._id}`}>
-                        <button className="btn p-0 border-none bg-base-100"><MdEdit size={20}/></button>
+                      <button className="btn p-0 border-none bg-base-100">
+                        <MdEdit size={20} />
+                      </button>
                     </Link>
-                    <button className="btn p-0 border-none bg-base-100"><MdDelete size={20}/></button>
+                    <button
+                      onClick={() => handleDelete(user._id)}
+                      className="btn p-0 border-none bg-base-100"
+                    >
+                      <MdDelete size={20} />
+                    </button>
                   </div>
                 </th>
               </tr>
